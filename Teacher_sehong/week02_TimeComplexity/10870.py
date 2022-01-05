3eb5aa6961fe579803e656d8760f58472c6240e7
sehoong0429 <tjsgh0801@naver.com>
2022-01-06 01:37:55 +0900

    fibonacci function

--- Teacher_sehong/week02_TimeComplexity/10870.py
+++ Teacher_sehong/week02_TimeComplexity/10870.py
@@ -1,1 +1,12 @@
 ##n이 주어졌을 때, n번째 피보나치 수를 구하기
+
+##피보나치 공식 Fn = Fn-1 + Fn-2 (n>=2)
+##피보나치 함수 만들기
+def fibonacci(n):
+    if n < 2 :
+        return n
+    else :
+        return fibonacci(n-1) + fibonacci(n-2)
+
+num = int(input())
+print(fibonacci(num))
